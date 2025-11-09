4cb68d8cab955914fbe0913892a382a7aa161a06
handdara <git@handdara.com>
2025-11-09 15:20:00 -0500

    Minor tweaks

--- hcfg/lua/hcfg/snippets/bash.lua
+++ hcfg/lua/hcfg/snippets/bash.lua
@@ -1,5 +1,6 @@
 ---@diagnostic disable: unused-local
 require('luasnip.session.snippet_collection').clear_snippets "bash"
+require('luasnip.session.snippet_collection').clear_snippets "sh"
 local ls = require 'luasnip'
 local s = ls.snippet
 local sn = ls.snippet_node
@@ -17,10 +18,26 @@ local function use(...)
     table.insert(S, s(...))
 end
 
-local function mkdatenode(fmtstr)
-    return f(function()
-        return os.date(fmtstr)
-    end)
-end
+
+local casetxt = [[
+case {1} in
+    {2}) 
+        {3}
+        ;;
+    *)
+        echo 'whoops!' >&2
+        exit -1 
+        ;;
+esac 
+]]
+use('case', fmt(casetxt,{
+    c(1, {
+        { t'$', i(1, "var") },
+        { t'$(', i(1, "cmd"), t')' },
+    }),
+    i(2,'cal|val*'),
+    i(3,[[echo 'found cal or something starting with "val"']]),
+}))
 
 ls.add_snippets("bash", S)
+ls.add_snippets("sh", S)
