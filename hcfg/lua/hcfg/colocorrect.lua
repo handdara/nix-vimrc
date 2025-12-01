e7fd8c856ad15f7f3f7db7a09cec1b07c158da9c
handdara <git@handdara.com>
2025-12-01 11:06:52 -0500

    __h_upd_home_mgr_nvim commited Mon Dec  1 11:06:52 AM EST 2025

--- hcfg/lua/hcfg/colocorrect.lua
+++ hcfg/lua/hcfg/colocorrect.lua
@@ -1,4 +1,6 @@
-local colo = vim.g.colors_name
-if colo == "lackluster" then
+local function iscolorscheme(x)
+    return x == vim.g.colors_name
+end
+if vim.iter({"lackluster","delek"}).any(iscolorscheme) then
     vim.cmd [[hi link markdownCode Special]]
 end
