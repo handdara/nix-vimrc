7167306448db7e3e2570f4960f0523b77f1be1e7
handdara <git@handdara.com>
2025-12-02 21:32:38 -0500

    __h_upd_home_mgr_nvim commited Tue Dec  2 09:32:38 PM EST 2025

--- hcfg/lua/hcfg/colocorrect.lua
+++ hcfg/lua/hcfg/colocorrect.lua
@@ -10,7 +10,21 @@ function M.correct()
     if vim.iter({"lackluster","delek"}):any(iscolorscheme) then
         vim.cmd [[hi link markdownCode Special]]
     elseif vim.iter({'eva01'}):any(iscolorscheme)  then
-        vim.cmd [[hi link markdownCode PreProc]]
+        vim.cmd [[
+        hi markdownLinkDelimiter guifg=#7a7a7a
+        hi htmlStrike cterm=strikethrough gui=strikethrough
+        hi htmlBoldItalic cterm=bold,italic gui=bold,italic
+        hi htmlBold cterm=bold gui=bold
+        hi htmlUnderline cterm=underline gui=underline
+        hi htmlItalic cterm=italic gui=italic
+        hi htmlBoldUnderline cterm=bold,underline gui=bold,underline
+        hi htmlUnderlineItalic cterm=underline,italic gui=underline,italic
+        hi htmlBoldUnderlineItalic cterm=bold,underline,italic gui=bold,underline,italic
+        hi link markdownCode PreProc
+        hi link markdownItalic htmlItalic
+        hi link markdownBoldItalic htmlBoldItalic
+        hi link markdownLinkTextDelimiter @markup.link
+        ]]
     end
 end
 
