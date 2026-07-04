ce3a58638f34d30572ab5c5c8db646783f33fdae
handdara <git@handdara.com>
2026-07-04 04:41:51 -0400

    __h_upd_home_mgr_nvim commited Sat Jul  4 04:41:51 AM EDT 2026

--- hcfg/lua/hcfg/commands.lua
+++ hcfg/lua/hcfg/commands.lua
@@ -20,3 +20,7 @@ end, { desc = 'Format current buffer with LSP' })
 vim.api.nvim_create_user_command('ToPDF', function(_)
     vim.cmd [[!nix-shell -p typst --run 'pandoc --pdf-engine=typst -V "mainfont=Tex Gyre Heros" -o "$(echo % | sed "s/\.\w\+/.pdf/")" %']]
 end, { desc = 'Format current buffer with LSP' })
+vim.api.nvim_create_user_command('PandocMarkdown', function()
+    vim.cmd [[!nix-shell -p typst --run 'pandoc --pdf-engine=typst --pdf-engine-opt=--root="$HOME/MEGA/ansible" --pdf-engine-opt=--creation-timestamp=$(date +\%s) --template="$HOME/.config/pandoc/templates/ansible.typ" -i % -t pdf -o /tmp/$(basename "%").pdf']]
+    vim.cmd [[!xdg-open /tmp/$(basename "%").pdf &]]
+end, {})
